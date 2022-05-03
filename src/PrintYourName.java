7734e3b3eb97c6d04e46865cd7c53e415c25e389
DavideGiusti87 <99611360+DavideGiusti87@users.noreply.github.com>
2022-05-03 12:36:08 +0200

    Esercizio3: Input and Output - 01

---
+++ src/PrintYourName.java
@@ -0,0 +1,18 @@
+/*
+Input and Output - 01
+Exercise: input-and-output-1
+create a class called PrintYourName
+provide the class PrintYourName with a main method:
+public static void main(String[] args) { ... }
+inside the main method write the code that prints your name
+Note: in the console, after your name, you will notice a message like the following:
+
+Process finished with exit code 0
+Don't worry, it means that you didn't break Java, so there were 0 errors :)
+ */
+public class PrintYourName {
+    public static void main(String[] args) {
+        System.out.println("Davide");
+    }
+
+}
